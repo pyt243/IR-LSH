dc5dec081cb4982fb018d8a01904048ebcda912d
iamkroot <kroot.patel@gmail.com>
2019-10-18 09:38:53 +0530

    Update LSH to return candidate set instead of bucket matrix

--- lsh.py
+++ lsh.py
@@ -1,60 +1,35 @@
-import os
-import numpy as np
 import pandas as pd
+from collections import defaultdict
+from itertools import combinations
 
 
-class Buckets:
+def get_candidates(sig_mat: pd.DataFrame, b, r):
     """
     Performs Loaclity Sensitive Hashing on generated Signatures.
+    Hashes columns based on bands
+    Args:
+        sig_mat (str) : Signature matrix generated using the LSH family
+        b (int) : Number of bands
+        r (int) : Number of rows per band
+    Returns:
+        set : The set of candidate pairs
     """
-
-    def __init__(self, path, b, r):
-        self.sig_path = path
-        self.b = b
-        self.r = r
-
-    def get_buckets(self):
-        """
-        Hashes columns based on bands
-
-        Returns:
-            list : Matrix which specifies the candidate pairs
-        """
-        Sig_Mat = pd.read_csv(self.sig_path)
-        col = len(Sig_Mat.columns) - 1
-        Bucket = [[0 for x in range(col)] for y in range(col)]
-        b = self.b
-        r = self.r
-        # for i in range(0,b):
-        #     for j in range(1,col):
-        #         a=Sig_Mat.iloc[i*r:(i+1)*r,j+1]
-        #         for k in range(j+1,col):
-        #             b=Sig_Mat.iloc[i*r:(i+1)*r,k+1]
-        #             if a.equals(b):
-        #                 Bucket[j][k]+=1
-        for i in range(0, b):
-            dict = {}
-            for j in range(1, col):
-                a = Sig_Mat.iloc[i * r : (i + 1) * r, j + 1]
-                num = 0
-                for k in a:
-                    num = num * 10 + k
-                if num in dict.keys():
-                    dict[num].append(j)
-                else:
-                    dict[num] = [j]
-            for k, v in dict.items():
-                for x in range(0, len(v)):
-                    for y in range(x + 1, len(v)):
-                        Bucket[v[x]][v[y]] += 1
-        return Bucket
+    num_cols = len(sig_mat.columns) - 1
+    candidates = set()
+    for i in range(0, b):
+        bucket = defaultdict(list)
+        for j in range(1, num_cols):
+            a = sig_mat.iloc[i * r : (i + 1) * r, j + 1]
+            col = ''.join(map(str, a))
+            bucket[col].append(j)
+        for values in bucket.values():
+            candidates.update(combinations(values, 2))
+    return candidates
 
 
 if __name__ == "__main__":
-    t = Buckets()
     ind_list = pd.read_csv("index.csv")
-    Bucket = t.get_buckets()
-    for i in range(1, 101):
-        for j in range(i + 1, 101):
-            if Bucket[i][j] > 0:
-                print(ind_list.iloc[i - 1, 2], ind_list.iloc[j - 1, 2])
+    sig_mat = pd.read_csv('cosine_signatures.csv')
+    candidates = get_candidates(sig_mat, 250, 20)
+    for pair in candidates:
+        print(ind_list.iloc[pair[0] - 1, 2], ind_list.iloc[pair[1] - 1, 2])
--- main.py
+++ main.py
@@ -1,42 +1,28 @@
-from lsh import Buckets
+from lsh import get_candidates
 import pandas as pd
 
-ind_list = pd.read_csv("index.csv")
-distances = ["cosine", "jaccard", "hamming"]
-br = [(250, 20), (500, 10), (31, 160)]
-# for d in distances:
-#     print("For "+d+" Signatures")
-#     pairDF = pd.read_csv("pairs/" + d + "_pairs.csv");
-#     pairs = pairDF.values;
-#     tpc, tp=0,0
-#     for p in pairs:
-#         i,j=p[1],p[2]
-#         print(ind_list.iloc[i-1,2],ind_list.iloc[j-1,2])
-#         tp+=1
-#         if i%5 == j%5:
-#             tpc+=1
-#     print("Precision :",tpc/tp)
-#     print("\n")
+
+def main():
+    ind_list = pd.read_csv("index.csv")
+    distances = ["cosine", "jaccard", "hamming"]
+    br = [(250, 20), (500, 10), (31, 160)]
+
+    for d, dist in enumerate(distances):
+        print("For " + dist.title() + " Signatures")
+        sig_mat = pd.read_csv(dist + "_signatures.csv")
+        candidates = get_candidates(sig_mat, br[d][0], br[d][1])
+        indDF = pd.DataFrame(candidates)
+        indDF.to_csv("pairs/" + dist + "_pairs.csv")
+        tpc, tp, tc = 0, 0, 100
+        print("Candidate pairs:")
+        for i, j in candidates:
+            tp = tp + 1
+            if j % 5 == i % 5:
+                tpc = tpc + 1
+            print(ind_list.iloc[i - 1, 2], ind_list.iloc[j - 1, 2])
+        print("Precision :", tpc / tp)
+        print("Recall : ", tpc / tc, end="\n\n")
 
 
-for d in range(len(distances)):
-    buc = Buckets(distances[d] + "_signatures.csv", br[d][0], br[d][1])
-    B = buc.get_buckets()
-    ind = []
-    tc = 100
-    tpc, tp = 0, 0
-    for i in range(1, 101):
-        for j in range(i + 1, 101):
-            if B[i][j] > 0:
-                tp = tp + 1
-                ind.append([i, j])
-                if j % 5 == i % 5:
-                    tpc = tpc + 1
-    print("For " + distances[d] + " Signatures")
-    print("Precision :", tpc / tp)
-    print("Recall : ", tpc / tc)
-    indDF = pd.DataFrame(ind)
-    indDF.to_csv("pairs/" + distances[d] + "_pairs.csv")
-    for i, j in ind:
-        print(ind_list.iloc[i - 1, 2], ind_list.iloc[j - 1, 2])
-    print("\n\n")
+if __name__ == '__main__':
+    main()
